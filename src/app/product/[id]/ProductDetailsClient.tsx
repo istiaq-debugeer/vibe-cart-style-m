f4b2be9bec62c67806ec050773b1c1d1c430dbe1
istiaq-debugeer <istiaqhasan93@gmail.com>
2025-12-18 10:40:28 +0600

    change ur

--- src/app/product/[id]/ProductDetailsClient.tsx
+++ src/app/product/[id]/ProductDetailsClient.tsx
@@ -27,7 +27,7 @@ export default function ProductDetailsClient({ id }: { id: string }) {
 
   // ✅ CORRECT Facebook Live URL (NO extra slash)
   const FACEBOOK_LIVE_URL =
-    "https://www.facebook.com/dailyshobji/videos/1514516669845709/";
+    "https://www.facebook.com/live/producer/1401822878076745/";
 
   useEffect(() => {
     if (product?.sizes?.length) {
